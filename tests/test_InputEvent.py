356d957879f0f40412e1d291df3ed62580ccbf3a
TTM4th <omantin60@gmail.com>
2024-05-18 03:23:13 +0900

    [update]Vscodeのテスト向けにテストコードを一部修正

--- tests/test_InputEvent.py
+++ tests/test_InputEvent.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
+from pygame import locals, event
+
+import sys
+import pathlib
+path = pathlib.Path('__file__')
+path /= '../' # 1つ上の階層を指す
+sys.path.append(str(path.resolve()))
 from megaman_python import InputEvent
-from pygame import locals
+
 class Test_InputEvent(TestCase):
 
     def setUp(self) -> None:
@@ -12,9 +19,15 @@ class Test_InputEvent(TestCase):
         mapper.AddMap(locals.K_x, InputEvent.InputKey.FIRE)
         mapper.AddMap(locals.K_z, InputEvent.InputKey.JUMP)
         self.test_mapper = mapper
+        self.test_keyInput = InputEvent.KeyInput(mapper.MapKeys,
+                                                 mapper.CreateKeyInputStateDictionary()
+                                                 )
         return super().setUp()
     
-    def test_createKeyInputStateDictionary(self):
+    """
+    setUpでKeyMapperに渡した値が 実際に入力したキー と ゲーム中の割当キー のペアで保存されているか
+    """
+    def test_Mapkeys(self):
         ans = {locals.K_UP : InputEvent.InputKey.Up,
                 locals.K_DOWN : InputEvent.InputKey.Down,
                 locals.K_LEFT : InputEvent.InputKey.LEFT,
@@ -23,3 +36,35 @@ class Test_InputEvent(TestCase):
                 locals.K_z : InputEvent.InputKey.JUMP}
         self.assertDictEqual(ans,self.test_mapper.MapKeys)
     
+    """
+    setUpでKeyMapperに渡した ゲーム中の割当キー と 入力状態（初期値：未入力）のペアが作成できるか
+    """
+    def test_CreateKeyInputStateDictionary(self):
+        ans = {InputEvent.InputKey.Up : InputEvent.InputState.NoneInput,
+                InputEvent.InputKey.Down : InputEvent.InputState.NoneInput,
+                InputEvent.InputKey.LEFT : InputEvent.InputState.NoneInput,
+                InputEvent.InputKey.RIGHT : InputEvent.InputState.NoneInput,
+                InputEvent.InputKey.FIRE : InputEvent.InputState.NoneInput,
+                InputEvent.InputKey.JUMP : InputEvent.InputState.NoneInput}
+        self.assertDictEqual(ans, self.test_mapper.CreateKeyInputStateDictionary())
+    
+    """
+    キー押下イベントを検知した検証対象のキーが未入力→入力開始状態に更新される
+    """
+    def test_InitKeyInput(self):
+        params = [
+            event.Event(locals.KEYDOWN, key = locals.K_UP),
+            event.Event(locals.KEYDOWN, key = locals.K_DOWN),
+            event.Event(locals.KEYDOWN, key = locals.K_LEFT),
+            event.Event(locals.KEYDOWN, key = locals.K_RIGHT),
+            event.Event(locals.KEYDOWN, key = locals.K_x),
+            event.Event(locals.KEYDOWN, key = locals.K_z)
+        ]
+        ans = {InputEvent.InputKey.Up : InputEvent.InputState.Start,
+                InputEvent.InputKey.Down : InputEvent.InputState.Start,
+                InputEvent.InputKey.LEFT : InputEvent.InputState.Start,
+                InputEvent.InputKey.RIGHT : InputEvent.InputState.Start,
+                InputEvent.InputKey.FIRE : InputEvent.InputState.Start,
+                InputEvent.InputKey.JUMP : InputEvent.InputState.Start}
+        self.test_keyInput.catchInput(params)
+        self.assertDictEqual(ans, self.test_keyInput.InputtedStates)
